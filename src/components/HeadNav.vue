844d3e523c1e1c70aa87b77e3e5089207d157933
AANovgorodov <aanovgorodov@beeline.ru>
2020-04-24 07:51:37 +0300

    fix navbar

--- src/components/HeadNav.vue
+++ src/components/HeadNav.vue
@@ -1,32 +1,53 @@
 <template>
+    <b-navbar  type="light" variant="light">
+     <b-nav-form>
+            <b-input-group class="mr-sm-2" prepend="Update every">
+                <b-form-select v-model="selected" :options="options"></b-form-select>
+            </b-input-group>
+            <b-button variant="outline-success" class="my-2 my-sm-0" type="submit">
+                            <b-icon icon="arrow-repeat" aria-hidden="true"></b-icon>                      
+            </b-button>
+    </b-nav-form>
    
-        <div class="row headnav">
-                <div class="navbar-nav mr-auto mt-2 mt-lg-0"></div>
-                <div  class="form-inline my-2 my-lg-0"> 
-                             <b-dropdown dropleft >
-                            <template v-slot:button-content>
-                              <b-icon icon="person-bounding-box" aria-hidden="true"></b-icon>&nbsp;Ivan
-                            </template>
-                            <b-dropdown-item href="#" disabled="" >Профиль</b-dropdown-item>
-                            <b-dropdown-item href="#"  disabled="" >Выход</b-dropdown-item>
-                        </b-dropdown>
-                </div>
-        </div>
+    <b-collapse id="nav-collapse" is-nav>
+        <b-nav-form>
+        
+           
+        </b-nav-form>
+      <b-navbar-nav class="ml-auto">
+     
+
+        <b-nav-item-dropdown right>
+          <template v-slot:button-content>
+           <b-icon icon="person-bounding-box" aria-hidden="true"></b-icon>&nbsp;Ivan
+          </template>
+                <b-dropdown-item href="#" disabled="" >Профиль</b-dropdown-item>
+                <b-dropdown-item href="#"  disabled="" >Выход</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
 </template>
 
 <script>
 export default {
-   name: "HeadNav"
+    name: "HeadNav",
+    data () {
+      return {
+         selected: 5,
+        options: [
+            { value: 5, text: '5 seconds' },
+            { value: 30, text: '30 seconds' },
+            { value: 60, text: '1 minute' },
+            { value: 0, text: 'Never' }
+        ],
+      } 
+    }
 }
 </script>
 
 
 <style>
-.headnav{
-    background-color: #FFF;
-padding:10px;
-height: 65px;
-    border-bottom: 1px solid rgba(0,0,0,.0625)
-}
+
 
 </style>
